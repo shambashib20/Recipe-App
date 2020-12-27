8ce3cc4463d1887b207c7770bc5d3ddbe0837a6a
Shambashib Majumdar <62923672+shambashib20@users.noreply.github.com>
2020-12-27 06:30:09 +0530

    Update App.js

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () =>{
   const APP_ID = "6adfadca";
-  const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09	";
+  const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09";
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}` ;
 

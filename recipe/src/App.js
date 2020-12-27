a4bbfd756424076ed3c5bc492a8d5f80fffc6f4a
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-27 06:28:29 +0530

    Declaration of API_KEY and API_IDs

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-const APP_ID = "6adfadca";
-const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09	";
+const App = () =>{
+  const APP_ID = "6adfadca";
+  const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09	";
 
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}` ;
+
+  return(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
 
 export default App;

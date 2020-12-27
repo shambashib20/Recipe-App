6dd48e9780be9ca250296f77e62851d328b1590c
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-27 06:13:05 +0530

    2nd commit

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,25 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const APP_ID = "6adfadca";
+const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09	";
+
 
 export default App;

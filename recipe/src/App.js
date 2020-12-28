4cb6885aa4ec2d5d7763e43b5432eea5c8dfb7a1
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-28 22:28:18 +0530

    minor changes.

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -5,22 +5,24 @@ const App = () =>{
   const APP_ID = "6adfadca";
   const APP_KEY = "2469d60cbb1802387e1b0d2e9b699f09";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+   
   
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() =>{
+  useEffect( () =>{
      console.log('Effect has been run');
-  });
+  }, []); 
   
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+  }
   return(
     <div className="App">
         <form className="search-form">
           <input className="search-bar" type="text"/>
           <button className="search-button" type="submit">Search</button>
         </form>
-        <h1 onClick ={() => setCounter(counter + 1)}>{counter}</h1>
-
+        
     </div>
   );
 };

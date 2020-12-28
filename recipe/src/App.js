c50e78141e73c762edd4ca12e8d00a9ca1670681
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-28 18:07:14 +0530

    implementated search bar

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -8,8 +8,11 @@ const App = () =>{
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}` ;
 
   return(
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text"/>
+          <button className="search-button" type="submit">Search</button>
+        </form>
     </div>
   )
 }

50c74c66e06dac92b3a9aaa295419c7d62ecdfce
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-29 21:37:56 +0530

    changes in styling+creating a separate css mod

--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Recipe = ({title, calories, image, ingredients }) => {
     return(
         <div>
-            <h1>{title}</h1>
+            <h1 classname="title">{title}</h1>
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>

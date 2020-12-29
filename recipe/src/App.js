256f3e0719246e6a49e34ee4c843aa5631d1415d
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-29 19:27:58 +0530

    optimised.

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -31,10 +31,11 @@ const App = () =>{
   
   const getSearch = e => {
      e.preventDefault();
+     setQuery(search);
   }
   return(
     <div className="App">
-        <form className="search-form">
+        <form onSubmit={getSearch} className="search-form">
           <input className="search-bar" type="text" value={search} onChange={updateSearch} />
           <button className="search-button" type="submit">Search</button>
         </form>

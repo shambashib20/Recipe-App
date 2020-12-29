e4d77df262208364507bbea564c9343f04087f76
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-29 21:56:09 +0530

    changes staged for styling.

--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -2,8 +2,8 @@ import React from 'react';
 import style from './recipe.module.css';
 const Recipe = ({title, calories, image, ingredients }) => {
     return(
-        <div>
-            <h1 classname={style.recipe}>{title}</h1>
+        <div className={style.recipe}>
+            <h1>{title}</h1>
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>

f03aa565537f0df226aa4eeae783940016c8acfc
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-29 20:06:13 +0530

    optimising.

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -36,7 +36,12 @@ const App = () =>{
   return(
     <div className="App">
         <form onSubmit={getSearch} className="search-form">
-          <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+          <input 
+            className="search-bar" 
+            type="text" 
+            value={search} 
+            onChange={updateSearch} 
+          />
           <button className="search-button" type="submit">Search</button>
         </form>
         {recipes.map(recipe => (

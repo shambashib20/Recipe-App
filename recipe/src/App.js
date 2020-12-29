5dac7d57d846f6bf427240253b3eaa81a7731599
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-12-29 11:36:51 +0530

    Sucessfully called the Edamam Api in these commits

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -16,6 +16,7 @@ const App = () =>{
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
   return(
     <div className="App">
@@ -23,6 +24,11 @@ const App = () =>{
           <input className="search-bar" type="text"/>
           <button className="search-button" type="submit">Search</button>
         </form>
+        {recipes.map(recipe => (
+          <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+          />
+        ))}
         
     </div>
   );
